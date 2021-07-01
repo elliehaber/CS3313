e5fa061e4dea5226ce3c635932592081f8b933c5
elliehaber <47250704+elliehaber@users.noreply.github.com>
2021-06-30 23:28:29 -0400

    Update Entity.h

--- P3/SDLSimple/SDLSimple/Entity.h
+++ P3/SDLSimple/SDLSimple/Entity.h
@@ -7,7 +7,7 @@
 #define GL_GLEXT_PROTOTYPES 1
 #include <SDL.h>
 #include <SDL_opengl.h>
-#include "lm/mat4x4.hpp"
+#include "glm/mat4x4.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "ShaderProgram.h"
 
